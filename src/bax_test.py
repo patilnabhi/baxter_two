466029c81ee1e7670e07c4db5f2ca57670ee8f7a
Alan Hong <alanhong2016@u.northwestern.edu>
2015-11-29 16:35:47 -0600

    test

---
+++ src/bax_test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+##Wait until system is completely initialized. Otherwise errors may be encountered
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import moveit_python as mip
+from baxter_interface import Limb, Gripper #, RobotEnable, CameraController, DigitalIO
+# import rospkg
+from os import system
+
+class RobotState:
+    def __init__(self):
+        pass
+        # self._robot=RobotEnable()
+        # self._rospack=rospkg.RosPack()
+        # self._baxter_tools=rospack.get_path('baxter_tools')
+    def enable(self):
+            system('python `rospack find baxter_tools`/scripts/enable_robot.py -e')
+        # system('python ' + _baxter_tools + '/scripts/enable_robot.py -e')
+        # self._robot.enable()  #ditched since it's success is contingent on other processes. It's safer to just incorporate the safeguards of already-existing scripts
+    def disable(self):
+            system('python `rospack find baxter_tools`/scripts/enable_robot.py -d')
+    def reset(self):
+            system('python `rospack find baxter_tools`/scripts/enable_robot.py -r')
+            # system('python ' + _baxter_tools + '/scripts/enable_robot.py -r')
+    def tuck(self):
+            system('python `rospack find baxter_tools`/scripts/tuck_arms.py -t')
+            # system('python ' + _baxter_tools + '/scripts/tuck_arms.py -t')
+    def untuck(self):
+            system('python `rospack find baxter_tools`/scripts/tuck_arms.py -t')
+            # system(_baxter_tools + '/scripts/tuck_arms.py -u')
+    def camera(self,action,cam):
+            #accepts "open" or "close" as action, and "head","left" or "right" as cam
+            if action == "open":
+                a = " -o"
+            elif action == "close":
+                a= " -c"
+            if cam == 'head':
+                c=" head_camera"
+            elif cam == 'right':
+                c=" right_hand_camera"
+            elif cam == 'left':
+                c=" left_hand_camera"
+            system('python `rospack find baxter_tools`/scripts/camera_control.py' +a +c)
+            # system(_baxter_tools + '/scripts/camera_control.py' +a +c)
+    def smile(self):
+        system('python `rospack find baxter_examples`/scripts/xdisplay_image.py --file=`rospack find bax_two`/images/honhon.png')
+
+    def calibrate_gripper(self):
+        pass
+        ##fill in here
+# #Display to Baxter's face. Publish: /robot/xdisplay 
+# #Cameras: Subscribe: /cameras/head_camera/image
+# import cv2
+# import cv_bridge
+# from sensor_msgs.msg import Image1
+
+
+
+# #To enable/disable/reset robot
+# import baxter_interface
+# from baxter_interface import CHECK_VERSION
+# rospy.init_node('rsdk_robot_enable')
+# rs = baxter_interface.RobotEnable(CHECK_VERSION)
+# #print rs.state() ; rs.enable() ; rs.disable() ; rs.reset()
+
+    
---
+++ src/image_feed.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import rospy
+
+# import cv2
+# import cv_bridge
+from sensor_msgs.msg import Image
+
+class ImageFeed:
+    def __init__(self):
+        rospy.init_node('display_feed')
+        self.sub = rospy.Subscriber('/cameras/right_hand_camera/image', Image, self.callback)
+        self.pub = rospy.Publisher('/robot/xdisplay', Image, queue_size=1) #, latch=True
+        
+    def callback(data):
+        self.pub.publish(data)
+
+if __name__ == '__main__':
+    try:
+        imgfeed = ImageFeed()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass    
